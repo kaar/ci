d2491c89caad56a14887dc1b5e2e3437a7d7bd34
Caspar Nettelbladt <caspar.n@gmail.com>
2023-10-12 23:40:29 +0200

    Refactor ci() function and add generate_commit() function

--- ci/main.py
+++ ci/main.py
@@ -20,35 +20,17 @@ def setup_logging():
     LOGGER.debug("Debug mode enabled")
 
 
-def ci():
-    setup_logging()
-    input_diff = git.cached_diff()
-    # input_diff = sys.stdin.read()
-
+def generate_commit(input_diff: str) -> str:
     COMMIT_INSTRUCTION = """
-You are going to receive a git diff and you are going to provide a git commit with the format: {Subject}\n\n{Message}.
-Subject is a short description of the change.
-For subject use imperative mood.
-Subject line is not allowed to be longer than 50 characters.
-Message is a more detailed description of the code change.
-Message is not needed for trivial changes.
-Be direct, try to eliminate filler words and phrases in these sentences.
-Shorter is always better.
+You will receive a git diff and respond with a git commit message.
+Limit the subject line to 50 characters.
 """
+
     system_message = {"role": "system", "content": COMMIT_INSTRUCTION}
-
-    # Don't include diffs that are too long
-    max_diff_length = 1000
-
-    history = []
-    for commit in git.latest_commits(5):
-        if len(commit.diff) > max_diff_length:
-            continue
-        history.append({"role": "user", "content": commit.diff})
-        history.append({"role": "assistant", "content": commit.message})
 
     input_message = {"role": "user", "content": input_diff}
 
+    history = []
     response = openai.ChatCompletion.create(
         model=DEFAULT_MODEL,
         messages=[
@@ -56,13 +38,17 @@ Shorter is always better.
             *history,
             input_message,
         ],
-        temperature=1,
+        temperature=0.2,
     )
-
-    print(response)
-
     commit_msg = response.choices[0].message.content
-    print(commit_msg)
+
+    return commit_msg
+
+
+def ci():
+    setup_logging()
+    input_diff = git.cached_diff()
+    commit_msg = generate_commit(input_diff)
     git.create_commit(commit_msg)
 
 
