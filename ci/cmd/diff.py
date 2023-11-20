cf44e6b6fae3f37aae31ef88a64aea6ef7a6ea23
Caspar Nettelbladt <caspar.n@gmail.com>
2023-11-20 13:57:08 +0100

    Fix highlighting of cached git diff
    The cached diff highlighting was not functioning correctly because
    the diff object was passed directly to the highlight.diff function.
    This commit fixes the issue by passing the text attribute of the diff
    object instead, ensuring that the diff is properly highlighted.

--- ci/cmd/diff.py
+++ ci/cmd/diff.py
@@ -3,5 +3,5 @@ from ci import git, highlight
 
 def cached() -> None:
     diff = git.cached_diff()
-    highlighed_diff = highlight.diff(diff)
+    highlighed_diff = highlight.diff(diff.text)
     print(highlighed_diff)
